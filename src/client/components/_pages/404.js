91006e44e0ded9182ff1e10ee3e683d2e8160de9
Benedikt Rauch <benediktrauch@users.noreply.github.com>
2017-06-23 13:22:33 +0200

    added 404 to router

--- src/client/components/_pages/404.js
+++ src/client/components/_pages/404.js
@@ -1,3 +1,13 @@
-/**
- * Created by Benedikt on 23.06.2017.
- */
+import React, {Component} from 'react'
+
+class NotFound extends Component {
+    render() {
+        return ( <div>
+                <h1>404</h1>
+                <h1>Not found</h1>
+            </div>
+        )
+    }
+}
+
+export default NotFound
