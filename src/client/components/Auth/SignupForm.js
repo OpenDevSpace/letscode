f8e3fe64eebaa4edac5d786e7566b3960d96dfb9
Benedikt Rauch <benediktrauch@users.noreply.github.com>
2017-06-29 17:10:42 +0200

    added stopImmediatePropagation

--- src/client/components/Auth/SignupForm.js
+++ src/client/components/Auth/SignupForm.js
@@ -14,6 +14,7 @@ class Signup extends Component {
 
             // Stop form from submitting normally
             event.preventDefault();
+            event.stopImmediatePropagation();
 
             // Get some values from elements on the page:
             var $form = $( this ),
